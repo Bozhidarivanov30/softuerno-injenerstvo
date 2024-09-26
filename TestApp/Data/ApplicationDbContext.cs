aa4fda605b53840fdd81cd4509673b8e6b865de1
Bozhidarivanov30 <bozhidarivanov20a@gmail.com>
2024-09-26 07:42:05 +0000

    mazna

---
+++ TestApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,5 @@
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options){}
+    public DbSet<Category> Categories{get; set;}
+}
---
+++ TestApp/Models/Category.cs
@@ -0,0 +1,3 @@
+public int Id{get;set;}
+public string Name {get; set;}
+public int DisplayOrder {get;set;}
